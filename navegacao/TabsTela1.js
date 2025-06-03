4f6d5cc7bd22391d552ca80335b6cc4e7b3cbab5
Maria Coelho <maeriacoelho@gmail.com>
2025-06-03 03:55:20 -0300

    realmente final

--- navegacao/TabsTela1.js
+++ navegacao/TabsTela1.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from '@expo/vector-icons';
 import Inicial from '../telas/Inicial';
 import User from '../telas/User';
 import Carrinho from '../telas/Carrinho';
@@ -13,7 +14,25 @@ export default function TabsTela1() {
     const Tab = createBottomTabNavigator();
   
     return (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Inicial') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if (route.name === 'Carrinho') {
+          iconName = focused ? 'cart' : 'cart-outline';
+        } else if (route.name === 'Úsuario') {
+          iconName = focused ? 'person' : 'person-outline';
+        }
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: '#FF8C42',
+      tabBarInactiveTintColor: 'gray',
+    })}
+  >
     <Tab.Screen
         name='Inicial'
         component={Inicial}
@@ -33,7 +52,7 @@ export default function TabsTela1() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF8C42',
     alignItems: 'center',
     justifyContent: 'center',
   },
